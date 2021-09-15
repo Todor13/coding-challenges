9acf58f9900fedf1bfc06be6961e5e30faf35112
todor <todorstaykovski@gmail.com>
2021-09-15 09:40:33 +0300

    Add n-th-tribonacci-number

---
+++ algorithms/python/dp/n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/n-th-tribonacci-number
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        arr = [0] * max(3, n + 1)
+        arr[0] = 0
+        arr[1] = 1
+        arr[2] = 1
+
+        for i in range(3, n + 1):
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3]
+
+        return arr[n]
