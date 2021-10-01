21f70ca3e6dbb1fe54c188f65996de44670ceefe
todor <todorstaykovski@gmail.com>
2021-10-01 12:06:34 +0300

    Add best-time-to-buy-and-sell-stock-ii

---
+++ algorithms/python/dp/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        b = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] > b:
+                profit += prices[i] - b
+
+            b = prices[i]
+
+        return profit
